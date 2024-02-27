4ad82596cf90297afc1846502feab14b600d6d67
Darko Bosak <darko.bosak@serengetitech.com>
2024-02-27 16:07:50 +0100

    Lookup tables added

---
+++ Application/Services/LiningService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class LiningService
+    {
+        private readonly ILiningRepository liningRepository;
+
+        public LiningService(ILiningRepository liningRepository)
+        {
+            this.liningRepository = liningRepository;
+        }
+        public async Task<IEnumerable<Top>> GetAllLinings()
+        {
+            return await liningRepository.GetAllAsync();
+        }
+
+        public async Task<Top> GetLiningById(int id)
+        {
+            return await liningRepository.GetByIdAsync(id);
+        }
+
+        public async Task<bool> AddLining(Lining lining)
+        {
+            await liningRepository.AddAsync(lining);
+
+            return true;
+        }
+
+        public async Task<bool> UpdateLining(Lining lining)
+        {
+            await liningRepository.UpdateAsync(lining);
+
+            return true;
+        }
+
+        public async Task<bool> DeleteLining(int id)
+        {
+            await liningRepository.DeleteAsync(id);
+
+            return true;
+        }
+    }
+}
---
+++ Infrastructure/Repository/SoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class SoleRepository : GenericRepository<Sole>, ISoleRepository
+    {
+        public SoleRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
