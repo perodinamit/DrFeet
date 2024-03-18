ac49865838d1a231943af363f223882e26d5cce3
perodinamit <matej.curcic@gmail.com>
2024-03-18 15:14:14 +0100

    Added validator for image size in update shoe razor page. (#9)
    Co-authored-by: Matej Curcic (Pontis) <pontis.matej.curcic@vwfspay.com>

---
+++ Domain/Validations/ShoeValidator.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validations
+{
+    public class ShoeValidator : AbstractValidator<Shoe>
+    {
+        public ShoeValidator()
+        {
+            RuleFor(x => x.ImageData)
+                .Must(BeValidImageSize)
+                .WithMessage("Image size must not exceed 2 MB.");
+        }
+
+        private bool BeValidImageSize(byte[] imageData)
+        {
+            if (imageData == null)
+            {
+                return true; // It's valid if no image is provided
+            }
+
+            const int maxSizeInBytes = 2097152; // 2 MB
+            return imageData.Length <= maxSizeInBytes;
+        }
+    }
+}
