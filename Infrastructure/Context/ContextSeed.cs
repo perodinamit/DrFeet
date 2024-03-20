3cb3c1eae3c057a6ac529c397ec4a74c6a2cc46b
Matej Curcic (Pontis) <pontis.matej.curcic@vwfspay.com>
2024-03-20 11:21:38 +0100

    Added new WebUI project (default now) with authorization

---
+++ Infrastructure/Context/ContextSeed.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+        }
+    }
+}
