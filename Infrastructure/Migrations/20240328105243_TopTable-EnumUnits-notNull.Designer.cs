bd4885f64d8385d487334227c6040a0858a76f4a
Matej Curcic (Pontis) <pontis.matej.curcic@vwfspay.com>
2024-03-28 15:15:06 +0100

    Added enum for Units and all that added to Tops.razor page

--- Infrastructure/Migrations/20240328105243_TopTable-EnumUnits-notNull.Designer.cs
+++ Infrastructure/Migrations/20240328105243_TopTable-EnumUnits-notNull.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240328105243_TopTable-EnumUnits-notNull")]
+    partial class TopTableEnumUnitsnotNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -193,7 +196,7 @@ namespace Infrastructure.Migrations
                     b.Property<decimal?>("NumberOfUnits")
                         .HasColumnType("decimal(18,2)");
 
-                    b.Property<int?>("Units")
+                    b.Property<int>("Units")
                         .HasColumnType("int");
 
                     b.HasKey("Id");
